38c5ee5965da5e522df95e54e13d6c30256c7c1a
Justin <justkreif@gmail.com>
2022-01-30 18:30:42 -0600

    Module 5 PyMongo Displays Database in Pytech

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,5 +1,8 @@
+# https://github.com/Krefails/csd-310
+# Justin Kreifels, 1-30-2022
 import pymongo
 from pymongo import MongoClient
+# Was getting an SSL CERT INVALID error. Fixed it by installing and importing Certifi.
 import certifi
 
 URI = "mongodb+srv://admin:admin@cluster0.1qv4w.mongodb.net/pytech?retryWrites=true&w=majority"
